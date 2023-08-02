b2d3743184b36322fb0140a02273b0d0bebb6801
Maxwell <maxwell.limingchi@gmail.com>
2023-08-03 01:22:59 +0800

    added spacial attention module

--- Code/module.py
+++ Code/module.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torchvision.models.mobilenetv2 import Conv2dNormActivation as ConvBNReLU
-
+from torch.ao.nn.intrinsic import ConvBn2d
 
 
 
@@ -58,32 +58,50 @@ class SPPM(nn.Module):
     
 class UAFM(nn.Module):
     def __init__(self,
-                 feature_high_channels,
-                 feature_low_channels,
-                 feature_high_shape,
-                 feature_low_shape,
+                 in_channels,
                  out_channels,
-                 AttentionModule='SAM'):
+                 feature_low_shape,
+                 AttentionModule = 'SAM'):
         super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.feature_low_shape = feature_low_shape
+        self.upsample = nn.Upsample(size=self.feature_low_shape, mode='bilinear')
 
-        self.upsample = nn.Upsample(size=feature_low_shape, mode='bilinear')
+        self.conv_xy_atten = nn.Sequential(
+            ConvBNReLU(
+                4, 2, kernel_size=3, padding=1, bias_attr=False),
+            ConvBn2d(
+                2, 1, kernel_size=3, padding=1, bias_attr=False)
+        )
+
         if AttentionModule == 'SAM':
-            self.AttentionModule = self.SAM()
+            self.AttentionModule = self.SAM
         elif AttentionModule == 'CAM':
-            self.AttentionModule = self.CAM()
+            self.AttentionModule = self.CAM
         else:
             raise "Attention module not found"
         
 
-    def SAM(self):
-        pass
+    def SAM(self, feature_up, feature_low):
+        feature_cat = torch.cat(
+            (torch.mean(feature_up, dim=1, keepdim=True),
+             torch.max(feature_up, dim=1, keepdim=True),
+             torch.mean(feature_low, dim=1, keepdim=True),
+             torch.max(feature_low, dim=1, keepdim=True)
+            ),
+            dim=1
+        )
+        alpha = nn.Sigmoid(self.conv_xy_atten(feature_cat))
+
+        return alpha
 
     def CAM(self):
         pass
 
     def forward(self, feature_high, feature_low):
         feature_up = self.upsample(feature_high)
-        alpha = self.AttentionModule(feature_high, feature_low)
+        alpha = self.AttentionModule(feature_up, feature_low)
         feature_out = feature_up * alpha + feature_low * (1 - alpha)
         return feature_out
 
